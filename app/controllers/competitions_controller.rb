3ba19e210fd2f1e141055e845cb6a85ab12b570b
jeehyekim <iam@jeehyekim.com>
2015-12-10 10:23:01 -0800

    competition show almost complete, added navbar, still trying to figure out tagging, got new competition form working but need to add more input fields for rest of information

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -2,11 +2,42 @@ class CompetitionsController < ApplicationController
 
   def index
     @competitions = Competition.all
-    render('index')
+    render :index
   end
 
   def new
+    @competition = Competition.new
+    render :new
+  end
 
+  def create 
+    @competition = Competition.create(competition_params)
+
+    if @competition.save
+      redirect_to competition_path(@competition)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @competition = Competition.find(params[:id])
+    render :show
+  end
+
+  # def edit
+  #   @competition = Competition.find(params[:id])
+  # end
+
+  # def update
+
+  # end
+
+
+  private
+
+  def competition_params
+    params.require(:competition).permit(:title, :description, :website, :image_url, :early_bird, :deadline, :notification)
   end
 
 end
