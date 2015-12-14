6c32cce3ccabce7f19b793f6f321ab5fb1c957bc
jeehyekim <iam@jeehyekim.com>
2015-12-13 21:18:20 -0800

    fix syntax error for heroku

--- app/jobs/subscribe_user_to_mailing_list_job.rb
+++ app/jobs/subscribe_user_to_mailing_list_job.rb
@@ -3,6 +3,6 @@ class SubscribeUserToMailingListJob < ActiveJob::Base
 
   def perform(user)
     gb = Gibbon::API.new
-    gb.lists.subscribe({:id => ENV["MAILCHIMP_LIST_ID", :email => {:email => user.email}, :double_optin => false})
+    gb.lists.subscribe({:id => ENV["MAILCHIMP_LIST_ID"], :email => {:email => user.email}, :double_optin => false})
   end
 end
